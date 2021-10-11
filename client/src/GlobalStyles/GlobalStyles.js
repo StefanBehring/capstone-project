26046769d4e9797358340895e2878470c1936f3d
StefanBehring <chadhz@gmx.de>
2021-10-11 17:27:05 +0200

    Add GlobalStyles

---
+++ client/src/GlobalStyles/GlobalStyles.js
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+
+    :root {
+        --color-lila-dark: #2a0944;
+        --color-lila: #3b185f;
+        --color-lila-light: #a12568;
+        --color-yellow: #fec260;
+        --color-green: #4caf50;
+        --color-red: #f44336;
+        --color-blue: #008cba;
+
+        --font-family: 'Roboto', sans-serif;
+    }
+
+    * {
+        box-sizing: border-box;
+    }
+
+    body {
+        background-color: var(--color-lila-dark);
+        color: #fff;
+        font-family: var(--font-family);
+        height: 100vh;
+        margin: 0;
+    }
+`
+
+export default GlobalStyles
