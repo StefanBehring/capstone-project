bc7aea05b7e9acc1dbcd5e20a08cc4a9aa713a91
StefanBehring <chadhz@gmx.de>
2021-10-11 17:27:56 +0200

    Add Footer component

---
+++ client/src/components/footer/Footer.stories.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Footer from './Footer'
+
+export default {
+  title: 'Component/Footer',
+  component: Footer,
+}
+
+const Template = args => <Footer {...args} />
+
+export const JustFooter = Template.bind({})
