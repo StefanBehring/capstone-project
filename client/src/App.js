95eaff730d6391784bb2a048d5f9b11aac0ae856
StefanBehring <chadhz@gmx.de>
2021-10-11 17:27:42 +0200

    Add LendingPage to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,37 @@
+import styled from 'styled-components'
+import Footer from './components/footer/Footer'
+import Header from './components/header/Header'
+import Home from './components/home/Home'
+
 function App() {
-  return <p>Hello World!</p>
+  return (
+    <Wrapper>
+      <Header />
+      <Main>
+        <Home />
+      </Main>
+      <Footer />
+    </Wrapper>
+  )
 }
+
+const Wrapper = styled.div`
+  justify-self: center;
+  display: grid;
+  grid-template-rows: 50px auto 50px;
+  height: 100vh;
+  margin: 0;
+  width: 375px;
+`
+
+const Main = styled.main`
+  background-color: var(--color-lila-light);
+  display: flex;
+  flex-direction: column;
+  justify-content: first baseline;
+  align-items: center;
+  margin: 0;
+  overflow: auto;
+`
 
 export default App
