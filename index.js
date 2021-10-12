46aa1f6812924e74540e35a2d7dd2f6aa3f3c73e
StefanBehring <chadhz@gmx.de>
2021-10-12 16:11:48 +0200

    Add morgan, helmet and cors to backend

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const morgan = require('morgan')
+const helmet = require('helmet')
+const cors = require('cors')
 const connectDatabase = require('./connectDatabase')
 const errorHandler = require('./errorHandler')
 require('dotenv').config()
@@ -10,6 +13,21 @@ const { PORT, MONGODB_URI } = process.env
 connectDatabase(MONGODB_URI)
 
 app.use(express.json())
+app.use(morgan('common'))
+app.use(helmet())
+
+// whitelist
+const whitelist = ['http://localhost:3000', 'http://localhost:3001']
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+}
+app.use(cors(corsOptions))
 
 app.use('/api/users', require('./routes/users'))
 app.use('/api/movies', require('./routes/movies'))
