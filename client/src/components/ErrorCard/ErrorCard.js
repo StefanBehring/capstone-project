42697fcf3f47887f637993ea348c988d16a8b8c3
StefanBehring <chadhz@gmx.de>
2021-10-12 10:00:18 +0200

    Edit color red for a11y

--- client/src/components/ErrorCard/ErrorCard.js
+++ client/src/components/ErrorCard/ErrorCard.js
@@ -13,7 +13,6 @@ const Wrapper = styled.div`
   background-color: var(--color-red);
   border: 2px solid var(--color-lila-dark);
   border-radius: 10px;
-  color: #000;
   display: flex;
   flex-direction: column;
   align-items: center;
