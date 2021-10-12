4938fc36f57231a0b530c0bc9061ea5a66781892
StefanBehring <chadhz@gmx.de>
2021-10-12 20:51:45 +0200

    Add AddMovieForm to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components/macro'
-import ErrorCard from './components/ErrorCard/ErrorCard'
+import AddMovieForm from './components/AddMovieForm/AddMovieForm'
+import ErrorCard from './components/Messages/ErrorCard/ErrorCard'
 import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import Home from './components/Home/Home'
@@ -10,14 +11,7 @@ function App() {
     <Wrapper>
       <Header />
       <Main>
-        <Home />
-        <ErrorCard title="Error" message="message" />
-        <MovieCard
-          title="Fight Club"
-          year="1999"
-          genre="Drama"
-          imgUrl="https://image.tmdb.org/t/p/w500/a26cQPRhJPX6GbWfQbvZdrrp9j9.jpg"
-        />
+        <AddMovieForm />
       </Main>
       <Footer />
     </Wrapper>
