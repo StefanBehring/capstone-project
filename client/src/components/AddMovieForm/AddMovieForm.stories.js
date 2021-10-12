674e1cf78b4ccecc520634d6f6f832322a255800
StefanBehring <chadhz@gmx.de>
2021-10-12 21:33:04 +0200

    Add AddMovieForm storybook

---
+++ client/src/components/AddMovieForm/AddMovieForm.stories.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import AddMovieForm from './AddMovieForm'
+
+export default {
+  title: 'Component/Admin/AddMovieForm',
+  component: AddMovieForm,
+}
+
+const Template = () => <AddMovieForm />
+
+export const AddMovieFormComplete = Template.bind({})
