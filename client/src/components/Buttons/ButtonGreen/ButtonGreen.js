6a1b7799f2d203e0889525e57d4e16780b4344e8
StefanBehring <chadhz@gmx.de>
2021-10-13 10:20:11 +0200

    Edit styled components macro import

--- client/src/components/Buttons/ButtonGreen/ButtonGreen.js
+++ client/src/components/Buttons/ButtonGreen/ButtonGreen.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled from 'styled-components/macro'
 
 const ButtonGreen = ({ message }) => {
   return <Button>{message}</Button>
--- client/src/components/Buttons/ButtonRed/ButtonRed.js
+++ client/src/components/Buttons/ButtonRed/ButtonRed.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled from 'styled-components/macro'
 
 const ButtonRed = ({ message }) => {
   return <Button>{message}</Button>
