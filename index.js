ec912c22cba17a6d56a7342b44b247f2592009d9
StefanBehring <chadhz@gmx.de>
2021-10-13 10:46:33 +0200

    Remove cors

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
 const helmet = require('helmet')
-const cors = require('cors')
 const connectDatabase = require('./connectDatabase')
 const errorHandler = require('./errorHandler')
 require('dotenv').config()
@@ -15,19 +14,6 @@ connectDatabase(MONGODB_URI)
 app.use(express.json())
 app.use(morgan('common'))
 app.use(helmet())
-
-// whitelist
-const whitelist = ['http://localhost:3000', 'http://localhost:3001']
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  },
-}
-app.use(cors(corsOptions))
 
 app.use('/api/users', require('./routes/users'))
 app.use('/api/movies', require('./routes/movies'))
