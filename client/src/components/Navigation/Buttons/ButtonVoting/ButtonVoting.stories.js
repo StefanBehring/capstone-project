99eade2dc445a668a72396f08da02e32bcf86ad8
StefanBehring <chadhz@gmx.de>
2021-10-15 15:18:31 +0200

    Add ButtonVoting Component, spec, storybook

---
+++ client/src/components/Navigation/Buttons/ButtonVoting/ButtonVoting.stories.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ButtonVoting from './ButtonVoting'
+
+export default {
+  title: 'Component/Navigation/Buttons/ButtonVoting',
+  component: ButtonVoting,
+}
+
+const Template = () => <ButtonVoting />
+
+export const ButtonVotingComplete = Template.bind({})
