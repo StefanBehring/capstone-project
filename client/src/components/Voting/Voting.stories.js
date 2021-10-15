0a2d8ef5e327985c8e292d4a7e58334a0e66f37b
StefanBehring <chadhz@gmx.de>
2021-10-15 12:00:04 +0200

    Add Voting Story

---
+++ client/src/components/Voting/Voting.stories.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Voting from './Voting'
+
+export default {
+  title: 'Component/Voting/Voting',
+  component: Voting,
+}
+
+const Template = () => <Voting />
+
+export const VotingComplete = Template.bind({})
