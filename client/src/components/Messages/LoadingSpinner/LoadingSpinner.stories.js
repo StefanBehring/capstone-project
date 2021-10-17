8905ee2dfb25a6a13acab8b1160ff62f83a4beb3
StefanBehring <chadhz@gmx.de>
2021-10-17 13:06:22 +0200

    Add LoadingSpinner component, storybook

---
+++ client/src/components/Messages/LoadingSpinner/LoadingSpinner.stories.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import LoadingSpinner from './LoadingSpinner'
+
+export default {
+  title: 'Component/Messages/LoadingSpinner',
+  component: LoadingSpinner,
+}
+
+const Template = () => <LoadingSpinner />
+
+export const LoadingSpinnerComplete = Template.bind({})
