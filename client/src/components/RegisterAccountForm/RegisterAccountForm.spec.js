186447acc2eb024f5bf1d98d969e28e5a588d427
StefanBehring <chadhz@gmx.de>
2021-10-18 14:36:05 +0200

    Edit getByText to getByLabelText

--- client/src/components/RegisterAccountForm/RegisterAccountForm.spec.js
+++ client/src/components/RegisterAccountForm/RegisterAccountForm.spec.js
@@ -9,16 +9,16 @@ describe('RegisterAccountForm', () => {
     const h2Element = screen.getAllByRole('heading', { level: 2 })
     expect(h2Element).toHaveLength(1)
 
-    const textUsername = screen.getByText('Username')
+    const textUsername = screen.getByLabelText('Username')
     expect(textUsername).toBeInTheDocument()
 
-    const textEmail = screen.getByText('E-Mail')
+    const textEmail = screen.getByLabelText('E-Mail')
     expect(textEmail).toBeInTheDocument()
 
-    const textPassword = screen.getByText('Password')
+    const textPassword = screen.getByLabelText('Password')
     expect(textPassword).toBeInTheDocument()
 
-    const textReType = screen.getByText('Re-Type')
+    const textReType = screen.getByLabelText('Re-Type')
     expect(textReType).toBeInTheDocument()
 
     const buttonElement = screen.getAllByRole('button')
