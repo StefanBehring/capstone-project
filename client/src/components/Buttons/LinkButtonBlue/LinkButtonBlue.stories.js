0d0cfaabee494a92d62ebfd7fa8ef1f8e33d19f1
StefanBehring <chadhz@gmx.de>
2021-10-18 14:34:28 +0200

    Add LinkButtonBlue component, spec, storybook

---
+++ client/src/components/Buttons/LinkButtonBlue/LinkButtonBlue.stories.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import LinkButtonBlue from './LinkButtonBlue'
+
+export default {
+  title: 'Component/Buttons/LinkButtonBlue',
+  component: LinkButtonBlue,
+}
+
+const Template = args => <LinkButtonBlue {...args} />
+
+export const LinkButtonBlueComplete = Template.bind({})
+LinkButtonBlueComplete.args = {
+  message: 'Register Now',
+  direction: '/register',
+}
