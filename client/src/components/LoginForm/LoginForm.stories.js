1ce4d12f533828c4cc64b217c51fc227c8be1291
StefanBehring <chadhz@gmx.de>
2021-10-19 11:36:06 +0200

    Add LoginForm spec, storybook

---
+++ client/src/components/LoginForm/LoginForm.stories.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import LoginForm from './LoginForm'
+
+export default {
+  title: 'Component/Formular/LoginForm',
+  component: LoginForm,
+}
+
+const Template = () => <LoginForm />
+
+export const LoginFormComplete = Template.bind({})
