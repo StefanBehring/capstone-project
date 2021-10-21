a8b4a760acf8928a925a15a18ba5af78c5df7a12
StefanBehring <chadhz@gmx.de>
2021-10-21 20:46:27 +0200

    Edit handlers

--- client/src/mocks/handlers.js
+++ client/src/mocks/handlers.js
@@ -1,8 +1,9 @@
 import { rest } from 'msw'
 
 export const handlers = [
-  rest.get('http://localhost:4000/api/movies/top', (req, res, ctx) => {
+  rest.get('/api/movies/top', (req, res, ctx) => {
     return res(
+      ctx.status(200),
       ctx.json([
         {
           _id: '61669c86242e85bee2082fe5',
@@ -19,12 +20,20 @@ export const handlers = [
           rating: 1373,
           tmdbId: '590',
         },
-        {
-          _id: '6166a60b1652fb89412cf888',
-          rating: 1369,
-          tmdbId: '580',
-        },
       ])
+    )
+  }),
+  rest.get('/api/tmdb/*', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        tmdbId: '580',
+        title: 'Jaws: The Revenge',
+        year: '1987',
+        genre: 'Adventure',
+        imgUrl:
+          'https://image.tmdb.org/t/p/w500/kGiaOztahZV2x7bil7sbk7fb6ob.jpg',
+      })
     )
   }),
 ]
