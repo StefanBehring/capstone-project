176425531ce65c966cf95148aa3a968f3c32dd08
StefanBehring <chadhz@gmx.de>
2021-10-21 20:46:46 +0200

    Add Toplist spec

--- client/src/components/Toplist/Toplist.spec.js
+++ client/src/components/Toplist/Toplist.spec.js
@@ -2,15 +2,15 @@ import { render, screen } from '@testing-library/react'
 import { MemoryRouter as Router } from 'react-router'
 import Toplist from './Toplist'
 
-xdescribe('Toplist', () => {
-  it('renders', () => {
+describe('Toplist', () => {
+  it('renders', async () => {
     render(
       <Router>
         <Toplist />
       </Router>
     )
 
-    const h2elements = screen.getAllByRole('heading', { level: 2 })
+    const h2elements = await screen.findAllByRole('heading', { level: 2 })
     expect(h2elements).toHaveLength(1)
   })
 })
