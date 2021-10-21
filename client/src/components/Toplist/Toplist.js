12bbcaa5bb44b020e20186412c9e4d5ad86e8f20
StefanBehring <chadhz@gmx.de>
2021-10-21 15:57:24 +0200

    Add useLoadMoviesTop hook

--- client/src/components/Toplist/Toplist.js
+++ client/src/components/Toplist/Toplist.js
@@ -1,46 +1,24 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
 import styled from 'styled-components/macro'
 import ErrorCard from '../Messages/ErrorCard/ErrorCard'
 import LoadingSpinner from '../Messages/LoadingSpinner/LoadingSpinner'
 import MovieCard from '../MovieCard/MovieCard'
+import useLoadMoviesTop from '../../hooks/useLoadMoviesTop'
 
 const Toplist = () => {
-  const [movies, setMovies] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
-  const [componentError, setComponentError] = useState('')
+  const moviesData = useLoadMoviesTop()
 
-  useEffect(() => {
-    const fetchMovies = async () => {
-      try {
-        const response = await axios.get(`/api/movies/top`)
-        if (!Array.isArray(response.data)) {
-          throw new Error('Invalid data')
-        }
-        setIsLoading(false)
-        setMovies(response.data)
-      } catch (error) {
-        console.error(error)
-        setIsLoading(false)
-        setComponentError({ message: error.message })
-      }
-    }
-
-    if (isLoading) fetchMovies()
-  }, [isLoading])
-
-  if (componentError !== '') {
-    return <ErrorCard title="Error" message={componentError.message} />
+  if (moviesData.errorMessage !== '') {
+    return <ErrorCard title="Error" message={moviesData.errorMessage} />
   }
 
-  if (isLoading) {
+  if (moviesData.isLoading) {
     return <LoadingSpinner />
   }
 
   return (
     <Wrapper>
       <h2>Toplist</h2>
-      {movies.map((movie, index) => {
+      {moviesData.movies.map((movie, index) => {
         return (
           <div key={movie._id}>
             <Ranking>#{index + 1}</Ranking>
---
+++ client/src/hooks/useLoadMoviesTop.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const useLoadMoviesTop = () => {
+  const [movieData, setMovieData] = useState({
+    movies: [],
+    isLoading: true,
+    errorMessage: '',
+  })
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await axios.get(`/api/movies/top`)
+        if (!Array.isArray(response.data)) {
+          throw new Error('Invalid data')
+        }
+        const newMovieData = {
+          movies: [...response.data],
+          isLoading: false,
+          errorMessage: '',
+        }
+        setMovieData(newMovieData)
+      } catch (error) {
+        console.error(error)
+        const newMovieData = {
+          movies: [],
+          isLoading: false,
+          errorMessage: error.message,
+        }
+        setMovieData(newMovieData)
+      }
+    }
+
+    fetchMovies()
+  }, [])
+
+  return movieData
+}
+
+export default useLoadMoviesTop
