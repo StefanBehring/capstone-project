a9a693f1944b3c1b1d8a28adf302d2384c9673f5
StefanBehring <chadhz@gmx.de>
2021-10-21 20:57:51 +0200

    Edit handler

--- client/src/mocks/handlers.js
+++ client/src/mocks/handlers.js
@@ -23,6 +23,28 @@ export const handlers = [
       ])
     )
   }),
+  rest.get('/api/movies/all', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([
+        {
+          _id: '61669c86242e85bee2082fe5',
+          rating: 1488,
+          tmdbId: '550',
+        },
+        {
+          _id: '61669b0f5642db42d61f716b',
+          rating: 1387,
+          tmdbId: '600',
+        },
+        {
+          _id: '6165d5b0a04769af7bdf9ec8',
+          rating: 1373,
+          tmdbId: '590',
+        },
+      ])
+    )
+  }),
   rest.get('/api/tmdb/*', (req, res, ctx) => {
     return res(
       ctx.status(200),
