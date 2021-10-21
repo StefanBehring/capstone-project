4ea36e31749988287ff25d5ec0982fb0e708345c
StefanBehring <chadhz@gmx.de>
2021-10-21 14:09:09 +0200

    Add useLoadMovieFromTmdb hook

---
+++ client/src/hooks/useLoadMovieFromTmdb.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const useLoadMovieFromTmdb = tmdbId => {
+  const [movie, setMovie] = useState({
+    tmdbId: '550',
+    imgUrl: 'https://image.tmdb.org/t/p/w500/a26cQPRhJPX6GbWfQbvZdrrp9j9.jpg',
+    title: 'Currently loading data',
+    year: '1999',
+    genre: 'Drama',
+    isLoading: true,
+  })
+
+  useEffect(() => {
+    const fetchMovie = async id => {
+      try {
+        const response = await axios.get(`/api/tmdb/${id}`)
+        let newMovie = response.data
+        newMovie.isLoading = false
+        setMovie(newMovie)
+      } catch (error) {
+        setMovie({
+          tmdbId: '550',
+          imgUrl:
+            'https://image.tmdb.org/t/p/w500/a26cQPRhJPX6GbWfQbvZdrrp9j9.jpg',
+          title: 'Fight Club',
+          year: '1999',
+          genre: 'Drama',
+          isLoading: false,
+        })
+        console.error(error)
+      }
+    }
+
+    fetchMovie(tmdbId)
+  }, [tmdbId])
+
+  return movie
+}
+
+export default useLoadMovieFromTmdb
