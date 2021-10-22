2b38e82a790b36495aea68c4610de57f0246bf17
StefanBehring <chadhz@gmx.de>
2021-10-22 11:46:24 +0200

    Add NotLoggedIn specs

--- client/src/components/NotLoggedIn/NotLoggedIn.spec.js
+++ client/src/components/NotLoggedIn/NotLoggedIn.spec.js
@@ -10,8 +10,8 @@ describe('NotLoggedIn', () => {
       </Router>
     )
 
-    const headerElement = screen.getAllByRole('heading', { level: 2 })
-    expect(headerElement).toHaveLength(1)
+    const headerElement = screen.getByRole('heading', { level: 2 })
+    expect(headerElement).toBeInTheDocument()
 
     const text1 = screen.getByText(
       'In order to view this page you have to be logged in.'
@@ -21,7 +21,32 @@ describe('NotLoggedIn', () => {
     const text2 = screen.getByText("Don't have an account yet?")
     expect(text2).toBeInTheDocument()
 
-    const linkElement = screen.getAllByRole('link')
-    expect(linkElement).toHaveLength(2)
+    const linkLogin = screen.getByText('Login')
+    expect(linkLogin).toBeInTheDocument()
+
+    const linkRegister = screen.getByText('Register now!')
+    expect(linkRegister).toBeInTheDocument()
+  })
+
+  it('renders 1 header element', () => {
+    render(
+      <Router>
+        <NotLoggedIn />
+      </Router>
+    )
+
+    const headerElements = screen.getAllByRole('heading', { level: 2 })
+    expect(headerElements).toHaveLength(1)
+  })
+
+  it('renders 2 link elements', () => {
+    render(
+      <Router>
+        <NotLoggedIn />
+      </Router>
+    )
+
+    const linkElements = screen.getAllByRole('link')
+    expect(linkElements).toHaveLength(2)
   })
 })
