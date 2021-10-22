29bedcf71b9dcfacb3b9ded726fa625d774c1f02
StefanBehring <chadhz@gmx.de>
2021-10-22 12:07:19 +0200

    Add EditPasswordForm specs

--- client/src/components/Profile/EditPasswordForm/EditPasswordForm.spec.js
+++ client/src/components/Profile/EditPasswordForm/EditPasswordForm.spec.js
@@ -1,13 +1,12 @@
 import { render, screen } from '@testing-library/react'
-
 import EditPasswordForm from './EditPasswordForm'
 
 describe('EditPasswordForm', () => {
   it('renders', () => {
     render(<EditPasswordForm />)
 
-    const h2Element = screen.getAllByRole('heading', { level: 2 })
-    expect(h2Element).toHaveLength(1)
+    const h2Element = screen.getByRole('heading', { level: 2 })
+    expect(h2Element).toBeInTheDocument()
 
     const textPassword = screen.getByLabelText('Password')
     expect(textPassword).toBeInTheDocument()
@@ -15,7 +14,56 @@ describe('EditPasswordForm', () => {
     const textReTypePassword = screen.getByLabelText('Re-Type Password')
     expect(textReTypePassword).toBeInTheDocument()
 
-    const buttonElement = screen.getAllByRole('button')
-    expect(buttonElement).toHaveLength(1)
+    const buttonElement = screen.getByRole('button')
+    expect(buttonElement).toBeInTheDocument()
+  })
+
+  it('renders only 1 heading', () => {
+    render(<EditPasswordForm />)
+
+    const h2element = screen.getAllByRole('heading', { level: 2 })
+    expect(h2element).toHaveLength(1)
+  })
+
+  it('renders heading with text "Edit Password"', () => {
+    render(<EditPasswordForm />)
+
+    const h2element = screen.getByRole('heading', { level: 2 })
+    expect(h2element).toHaveTextContent('Edit Password')
+  })
+
+  it('has an input field "Password" with the type "password"', () => {
+    render(<EditPasswordForm />)
+
+    const inputElement = screen.getByLabelText('Password')
+    expect(inputElement).toHaveAttribute('type', 'password')
+  })
+
+  it('has an input field "Password" which is "required"', () => {
+    render(<EditPasswordForm />)
+
+    const inputElement = screen.getByLabelText('Password')
+    expect(inputElement).toBeRequired()
+  })
+
+  it('has an input field "Re-Type Password" with the type "password"', () => {
+    render(<EditPasswordForm />)
+
+    const inputElement = screen.getByLabelText('Re-Type Password')
+    expect(inputElement).toHaveAttribute('type', 'password')
+  })
+
+  it('has an input field "Re-Type Password" which is "required"', () => {
+    render(<EditPasswordForm />)
+
+    const inputElement = screen.getByLabelText('Re-Type Password')
+    expect(inputElement).toBeRequired()
+  })
+
+  it('has 1 button element', () => {
+    render(<EditPasswordForm />)
+
+    const buttonElements = screen.getAllByRole('button')
+    expect(buttonElements).toHaveLength(1)
   })
 })
