3e8108bd206ed2792bfd3900fd88b25adc0b700b
StefanBehring <chadhz@gmx.de>
2021-10-22 10:43:41 +0200

    Add AddMovieForm specs

--- client/src/components/AddMovieForm/AddMovieForm.spec.js
+++ client/src/components/AddMovieForm/AddMovieForm.spec.js
@@ -14,4 +14,32 @@ describe('AddMovieForm', () => {
     const buttonElement = screen.getByRole('button')
     expect(buttonElement).toBeInTheDocument()
   })
+
+  it('renders heading with text "Add Movie"', () => {
+    render(<AddMovieForm />)
+
+    const h2element = screen.getByRole('heading', { level: 2 })
+    expect(h2element).toHaveTextContent('Add Movie')
+  })
+
+  it('renders only 1 heading', () => {
+    render(<AddMovieForm />)
+
+    const h2element = screen.getAllByRole('heading', { level: 2 })
+    expect(h2element).toHaveLength(1)
+  })
+
+  it('has an input field "TMDB Id" with the type "text"', () => {
+    render(<AddMovieForm />)
+
+    const inputElement = screen.getByLabelText('TMDB Id')
+    expect(inputElement).toHaveAttribute('type', 'text')
+  })
+
+  it('has an input field "TMDB Id" which is "required"', () => {
+    render(<AddMovieForm />)
+
+    const inputElement = screen.getByLabelText('TMDB Id')
+    expect(inputElement).toBeRequired()
+  })
 })
