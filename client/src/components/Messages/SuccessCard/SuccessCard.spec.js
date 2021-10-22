742674c04334b85f6d21ab66af903b5d398265fe
StefanBehring <chadhz@gmx.de>
2021-10-22 11:14:38 +0200

    Add SuccessCard specs

--- client/src/components/Messages/SuccessCard/SuccessCard.spec.js
+++ client/src/components/Messages/SuccessCard/SuccessCard.spec.js
@@ -3,13 +3,26 @@ import SuccessCard from './SuccessCard'
 
 describe('SuccessCard', () => {
   it('renders', () => {
-    const message = 'It was a success!'
-    render(<SuccessCard title="Title" message={message} />)
+    render(<SuccessCard title="Title" message="It was a success!" />)
 
     const h3Element = screen.getByRole('heading', { level: 3 })
     expect(h3Element).toBeInTheDocument()
 
-    const textMessage = screen.getByText(message)
+    const textMessage = screen.getByText('It was a success!')
     expect(textMessage).toBeInTheDocument()
+  })
+
+  it('renders only 1 h3 element', () => {
+    render(<SuccessCard title="Title" message="It was a success!" />)
+
+    const h3Elements = screen.getAllByRole('heading', { level: 3 })
+    expect(h3Elements).toHaveLength(1)
+  })
+
+  it('renders "Title" in the h3 element', () => {
+    render(<SuccessCard title="Title" message="It was a success!" />)
+
+    const h3Element = screen.getByRole('heading', { level: 3 })
+    expect(h3Element).toHaveTextContent('Title')
   })
 })
