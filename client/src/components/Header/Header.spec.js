2b860caa61d1c9bb95408c990c09ec87fe032a69
StefanBehring <chadhz@gmx.de>
2021-10-22 10:52:48 +0200

    Add Header specs

--- client/src/components/Header/Header.spec.js
+++ client/src/components/Header/Header.spec.js
@@ -5,7 +5,21 @@ describe('Header', () => {
   it('renders', () => {
     render(<Header />)
 
-    const h1element = screen.getByRole('heading')
+    const h1element = screen.getByRole('heading', { level: 1 })
     expect(h1element).toBeInTheDocument()
+  })
+
+  it('renders the text "Rate The Movie"', () => {
+    render(<Header />)
+
+    const h1element = screen.getByRole('heading', { level: 1 })
+    expect(h1element).toHaveTextContent('Rate The Movie')
+  })
+
+  it('renders only 1 heading', () => {
+    render(<Header />)
+
+    const headingElements = screen.getAllByRole('heading')
+    expect(headingElements).toHaveLength(1)
   })
 })
