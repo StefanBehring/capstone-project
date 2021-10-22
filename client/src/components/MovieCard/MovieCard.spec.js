6727567bdd4202b5b52bc5d6555d7b23e90edeaa
StefanBehring <chadhz@gmx.de>
2021-10-22 11:18:30 +0200

    Add MovieCard specs

--- client/src/components/MovieCard/MovieCard.spec.js
+++ client/src/components/MovieCard/MovieCard.spec.js
@@ -6,8 +6,8 @@ describe('MovieCard', () => {
   it('renders', async () => {
     render(<MovieCard tmdbId="550" />)
 
-    const h3Element = await screen.findAllByRole('heading', { level: 3 })
-    expect(h3Element).toHaveLength(1)
+    const h3Element = await screen.findByRole('heading', { level: 3 })
+    expect(h3Element).toBeInTheDocument()
 
     const imgElement = screen.getByAltText('')
     expect(imgElement).toBeInTheDocument()
@@ -20,5 +20,19 @@ describe('MovieCard', () => {
 
     const textGenre = screen.getByText('Adventure')
     expect(textGenre).toBeInTheDocument()
+  })
+
+  it('renders 1 h3 element', async () => {
+    render(<MovieCard tmdbId="550" />)
+
+    const h3Elements = await screen.findAllByRole('heading', { level: 3 })
+    expect(h3Elements).toHaveLength(1)
+  })
+
+  it('renders 1 img element', async () => {
+    render(<MovieCard tmdbId="550" />)
+
+    const imgElements = await screen.findAllByRole('img')
+    expect(imgElements).toHaveLength(1)
   })
 })
