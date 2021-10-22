84cf0e0ac1fdd7bae2ec1729f47b63cdf26c94ef
StefanBehring <chadhz@gmx.de>
2021-10-22 10:34:30 +0200

    Add Link specs

--- client/src/components/Buttons/LinkButtonBlue/LinkButtonBlue.spec.js
+++ client/src/components/Buttons/LinkButtonBlue/LinkButtonBlue.spec.js
@@ -10,7 +10,29 @@ describe('LinkButtonBlue', () => {
       </Router>
     )
 
-    const linkElement = screen.getAllByRole('link')
-    expect(linkElement).toHaveLength(1)
+    const linkElement = screen.getByRole('link')
+    expect(linkElement).toBeInTheDocument()
+  })
+
+  it('renders only 1 link element', () => {
+    render(
+      <Router>
+        <LinkButtonBlue message="Register now" direction="/register" />
+      </Router>
+    )
+
+    const linkElements = screen.getAllByRole('link')
+    expect(linkElements).toHaveLength(1)
+  })
+
+  it('renders a link with text "Register now"', () => {
+    render(
+      <Router>
+        <LinkButtonBlue message="Register now" direction="/register" />
+      </Router>
+    )
+
+    const linkElement = screen.getByRole('link')
+    expect(linkElement).toHaveTextContent('Register now')
   })
 })
