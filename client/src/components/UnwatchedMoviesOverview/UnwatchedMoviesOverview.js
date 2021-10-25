0a3f46d0cbca0c92c940f9ec7acea768216d1753
StefanBehring <chadhz@gmx.de>
2021-10-25 16:32:04 +0200

    Add UnwatchedMoviesOverview component

---
+++ client/src/components/UnwatchedMoviesOverview/UnwatchedMoviesOverview.js
@@ -0,0 +1,40 @@
+import styled from 'styled-components/macro'
+import { Redirect } from 'react-router'
+import LoadingSpinner from '../Messages/LoadingSpinner/LoadingSpinner'
+import useFetchUnwatchedMovies from '../../hooks/useFetchUnwatchedMovies'
+import UnwatchedMovie from './UnwatchedMovie/UnwatchedMovie'
+
+const UnwatchedMoviesOverview = () => {
+  const unwatchedMoviesData = useFetchUnwatchedMovies()
+
+  if (unwatchedMoviesData.isLoading) {
+    return <LoadingSpinner />
+  }
+
+  if (unwatchedMoviesData.errorMessage !== '') {
+    alert('Redirecting to notLoggedIn')
+    return <Redirect to="/notLoggedIn" />
+  }
+
+  return (
+    <UnwatchedMoviesWrapper>
+      <h2>Unwatched Movies</h2>
+      {unwatchedMoviesData.infoData.map(movie => {
+        return <UnwatchedMovie key={movie} unwatchedMovieId={movie} />
+      })}
+    </UnwatchedMoviesWrapper>
+  )
+}
+
+const UnwatchedMoviesWrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h2 {
+    color: var(--color-black);
+    margin-bottom: 0.3rem;
+  }
+`
+
+export default UnwatchedMoviesOverview
