816a9d35519b1e01fa994039da8c12bbee9a0de5
StefanBehring <chadhz@gmx.de>
2021-10-27 15:18:35 +0200

    Add new route to services

--- client/src/services/getMovieById.js
+++ client/src/services/getMovieById.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const getMovieById = movieId => {
-  return axios.get(`/api/movies/${movieId}`).then(response => response.data)
+  return axios.get(`/api/movie/${movieId}`).then(response => response.data)
 }
 
 export default getMovieById
--- client/src/services/postNewMovie.js
+++ client/src/services/postNewMovie.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const postNewMovie = newMovie => {
-  return axios.post(`/api/movies`, newMovie)
+  return axios.post(`/api/movie`, newMovie)
 }
 
 export default postNewMovie
