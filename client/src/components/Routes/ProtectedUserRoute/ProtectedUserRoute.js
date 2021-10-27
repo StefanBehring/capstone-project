ca17d57ecb698139ef818bd33532bd0e73ad6e03
StefanBehring <chadhz@gmx.de>
2021-10-27 11:07:30 +0200

    Add ProtectedUserRoute

---
+++ client/src/components/Routes/ProtectedUserRoute/ProtectedUserRoute.js
@@ -0,0 +1,34 @@
+import { Route, Redirect } from 'react-router-dom'
+
+const ProtectedUserRoute = ({
+  isLoggedIn,
+  component: Component,
+  onLogin,
+  ...rest
+}) => {
+  return (
+    <Route
+      {...rest}
+      render={props => {
+        if (!isLoggedIn && onLogin) {
+          return <Component onLogin={onLogin} {...props} />
+        } else if (!isLoggedIn) {
+          return <Component {...props} />
+        } else {
+          return (
+            <Redirect
+              to={{
+                pathname: '/',
+                state: {
+                  from: props.location,
+                },
+              }}
+            />
+          )
+        }
+      }}
+    />
+  )
+}
+
+export default ProtectedUserRoute
