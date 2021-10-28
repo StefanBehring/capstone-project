6f4e622e3f176e222c9d90314e6f609137f7b1ca
StefanBehring <chadhz@gmx.de>
2021-10-28 13:42:28 +0200

    Edit Home

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -6,10 +6,17 @@ import LinkButtonBlue from '../Buttons/LinkButtonBlue/LinkButtonBlue'
 const Home = () => {
   return (
     <CardWrapper>
-      <H2>Welcome to rate the movie</H2>
+      <H2>Welcome to be objective</H2>
       <ParagraphCenter>
-        On this app you will have the option to rate movies in a more accurate
-        way than on other rating forms.
+        On this app you will have the option to rate movies.
+      </ParagraphCenter>
+      <ParagraphCenter>
+        You don't do it in the typical 5-star fashion way. You decide which
+        movie you like more!
+      </ParagraphCenter>
+      <ParagraphCenter>
+        This way we get a way better understanding of which one really is the
+        best movie.
       </ParagraphCenter>
       <ParagraphCenter>Have fun!</ParagraphCenter>
       <LinkButtonBlue message="Login" direction="/login" />
