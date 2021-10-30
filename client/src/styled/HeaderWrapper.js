e94fe44b93de14e19839cada8149e9f6ece41847
StefanBehring <chadhz@gmx.de>
2021-10-30 17:52:59 +0200

    Edit HeaderWrapper styling

--- client/src/styled/HeaderWrapper.js
+++ client/src/styled/HeaderWrapper.js
@@ -4,15 +4,9 @@ const HeaderWrapper = styled.header`
   background-color: var(--color-primary-heavy);
   color: var(--color-white-default);
   display: flex;
-  justify-content: space-around;
   align-items: center;
-
-  img {
-    margin: 0;
-    padding: 0;
-    height: 50px;
-    width: auto;
-  }
+  padding-top: 1rem;
+  padding-left: 1.5rem;
 `
 
 export default HeaderWrapper
