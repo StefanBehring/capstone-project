4dee2e28683d2fdb62f369c14c078db9a945cbc2
StefanBehring <chadhz@gmx.de>
2021-10-30 18:56:10 +0200

    Edit storybook VotingArea

--- client/src/components/Voting/VotingArea/VotingArea.stories.js
+++ client/src/components/Voting/VotingArea/VotingArea.stories.js
@@ -1,3 +1,4 @@
+import { action } from '@storybook/addon-actions'
 import VotingArea from './VotingArea'
 
 export default {
@@ -5,6 +6,12 @@ export default {
   component: VotingArea,
 }
 
-const Template = () => <VotingArea />
+const Template = args => <VotingArea {...args} />
 
 export const VotingAreaComplete = Template.bind({})
+VotingAreaComplete.args = {
+  onVoteClick: action(() => console.log('VOTE')),
+  onUnknownMovieClick: action(() => console.log('UNKNOWN')),
+  firstMovieTmdbId: '550',
+  secondMovieTmdbId: '560',
+}
