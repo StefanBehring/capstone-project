f3512596ed7c402b6ac7f5a63be833a6471cfd32
StefanBehring <chadhz@gmx.de>
2021-10-31 13:00:20 +0100

    Add MovieDetails Component, spec, storybook

---
+++ client/src/components/MovieDetails/MovieDetails.stories.js
@@ -0,0 +1,10 @@
+import MovieDetails from './MovieDetails'
+
+export default {
+  title: 'Component/MovieDetails',
+  component: MovieDetails,
+}
+
+const Template = () => <MovieDetails />
+
+export const MovieDetailsComplete = Template.bind({})
